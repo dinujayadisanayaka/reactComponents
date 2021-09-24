50f332d874b652c173de00f2921a4fef3ceaae5f
dmnddisanayaka <dmnddisanayaka@gmail.com>
2021-09-24 23:41:38 +0530

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Box, Grid, Container, Typography } from '@mui/material';
+import ChartjsPie from '../components/chart/chartjsPie';
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            wchart:1
+        };
+    }
     render() {
         return (
-            <div className="container">
-                <div>
-                    Love u
-                </div>
-            </div>
+               <Container maxWidth="xl">
+        <Box sx={{ pb: 5 }}>
+          <Typography variant="h4">Hi, Welcome back</Typography>
+        </Box>
+        <Grid container spacing={3}>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <ChartjsPie/>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            Love
+          </Grid>
+
+          {this.state.wchart ?  <Grid item xs={12} sm={6} md={6}>
+          <ChartjsPie/>
+          </Grid> : 'not'}
+         
+        </Grid>
+      </Container>
+            // </div>
         )
     }
 }
+
+
+
