24046d3ca7b27b67d83ea16afc4d023b2e666f3a
dmnddisanayaka <dmnddisanayaka@gmail.com>
2021-10-04 18:38:11 +0530

    A C

--- src/components/chart/chartjsPie.js
+++ src/components/chart/chartjsPie.js
@@ -1,75 +1,35 @@
-import React, { Component } from 'react';
-import {Canvas} from "canvas";
+import React, { useState } from "react";
+import AreaChart from "./AreaChart";
 
+const points = [
+  { x: 0, y: 100 },
+  { x: 20, y: 20 },
+  { x: 40, y: 50 },
+  { x: 60, y: 20 },
+  { x: 80, y: 50 },
+  { x: 100, y: 20 }
+];
 
-export default class ChartjsPie extends Component {
-  render() {
-    const options = {
-      theme: "light2",
-      title: {
-        text: "Comparison of Exchange Rates - 2017"
-      },
-      subtitles: [{
-        text: "GBP & USD to INR"
-      }],
-      axisY: {
-        prefix: "₹"
-      },
-      toolTip: {
-        shared: true
-      },
-      data: [
-        {
-          type: "area",
-          name: "GBP",
-          showInLegend: true,
-          xValueFormatString: "MMM YYYY",
-          yValueFormatString: "₹#,##0.##",
-          dataPoints: [
-            { x: new Date("2017- 01- 01"), y: 84.927},
-            { x: new Date("2017- 02- 01"), y: 82.609},
-            { x: new Date("2017- 03- 01"), y: 81.428},
-            { x: new Date("2017- 04- 01"), y: 83.259},
-            { x: new Date("2017- 05- 01"), y: 83.153},
-            { x: new Date("2017- 06- 01"), y: 84.180},
-            { x: new Date("2017- 07- 01"), y: 84.840},
-            { x: new Date("2017- 08- 01"), y: 82.671},
-            { x: new Date("2017- 09- 01"), y: 87.496},
-            { x: new Date("2017- 10- 01"), y: 86.007},
-            { x: new Date("2017- 11- 01"), y: 87.233},
-            { x: new Date("2017- 12- 01"), y: 86.276}
-          ]
-        },
-        {
-          type: "area",
-          name: "USD",
-          showInLegend: true,
-          xValueFormatString: "MMM YYYY",
-          yValueFormatString: "₹#,##0.##",
-          dataPoints: [
-            { x: new Date("2017- 01- 01"), y: 67.515},
-            { x: new Date("2017- 02- 01"), y: 66.725},
-            { x: new Date("2017- 03- 01"), y: 64.86},
-            { x: new Date("2017- 04- 01"), y: 64.29},
-            { x: new Date("2017- 05- 01"), y: 64.51},
-            { x: new Date("2017- 06- 01"), y: 64.62},
-            { x: new Date("2017- 07- 01"), y: 64.2},
-            { x: new Date("2017- 08- 01"), y: 63.935},
-            { x: new Date("2017- 09- 01"), y: 65.31},
-            { x: new Date("2017- 10- 01"), y: 64.75},
-            { x: new Date("2017- 11- 01"), y: 64.49},
-            { x: new Date("2017- 12- 01"), y: 63.84}
-          ]
-        }
-      ]
-    }
-    return (
+function ChartjsPie() {
+  const [svgWidth, setSvgWidth] = useState(200);
+  const onSliderChange = (event) => setSvgWidth(event.target.value);
+  return (
+      <>
         <div>
-          <Canvas options = {options}
-              /* onRef={ref => this.chart = ref} */
-          />
-          {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+          <span>Current Width {svgWidth}</span>
         </div>
-    );
-  }
+        <div>
+          <input
+              type="range"
+              min="100"
+              max="800"
+              value={svgWidth}
+              onChange={onSliderChange}
+          />
+        </div>
+        <AreaChart width={svgWidth} points={points} />
+      </>
+  );
 }
+
+export default ChartjsPie;
