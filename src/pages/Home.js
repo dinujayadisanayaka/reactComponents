08223155a6f23b7e04c9054f9942acd6e35680d0
dmnddisanayaka <dmnddisanayaka@gmail.com>
2021-10-10 22:10:28 +0530

    CardSample

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,30 +23,27 @@ export default class Home extends Component {
         <Box sx={{ pb: 5 }}>
           <Typography variant="h4">Hi, Welcome back</Typography>
         </Box>
-        <Grid container spacing={3}>
-          <Grid item xs={4}>
-           <ChartjsPie/>
-          </Grid>
-          <Grid item xs={8} >
-            <AreaChar/>
-          </Grid>
-          <Grid item xs={12}>
-            <DoubleBargraph/>
-          </Grid>
+        {/*<Grid container spacing={3}>*/}
+        {/*  <Grid item xs={4}>*/}
+        {/*   <ChartjsPie/>*/}
+        {/*  </Grid>*/}
+        {/*  <Grid item xs={8} >*/}
+        {/*    <AreaChar/>*/}
+        {/*  </Grid>*/}
+        {/*  <Grid item xs={12}>*/}
+        {/*    <DoubleBargraph/>*/}
+        {/*  </Grid>*/}
 
-          <Grid item xs={8}>
-            <LineChart/>
-          </Grid>
+        {/*  <Grid item xs={8}>*/}
+        {/*    <LineChart/>*/}
+        {/*  </Grid>*/}
 
 
-            <Grid item xs={8} sm={6} md={3}>
-                <PieChart2/>
-            </Grid>
+        {/*    <Grid item xs={8} sm={6} md={3}>*/}
+        {/*        <PieChart2/>*/}
+        {/*    </Grid>*/}
 
-          {/*{this.state.wchart ?  <Grid item xs={12} sm={6} md={6}>*/}
-          {/*<ChartjsPie/>*/}
-          {/*</Grid> : 'not'}*/}
-        </Grid>
+        {/*</Grid>*/}
     </Container>
             </div>
         )
