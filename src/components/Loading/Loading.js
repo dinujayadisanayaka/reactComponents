9aabf5163cf33d918d23b707a0f623519b002aa4
dmnddisanayaka <dmnddisanayaka@gmail.com>
2021-10-10 15:54:54 +0530

    Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,41 @@
+import React from 'react';
+// import { makeStyles } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Card } from '@material-ui/core';
+import logo from '../assets/images/logo.png';
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//     paddingTop: '1em',
+//     paddingBottom: '3.5em'
+//   },
+//   aboutUsLeftSection: {
+//     paddingRight: '50px'
+//   } }));
+
+function Loading () {
+//   const classes = useStyles();
+    return (
+        <Card style = {{ backgroundColor: '#282C40', color: '#F97A09' }}>
+            <Grid
+                container
+                spacing={0}
+                direction='column'
+                alignItems='center'
+                justify='center'
+                style={{ minHeight: '100vh' }}
+            >
+                <Grid item xs={3}>
+                    <image alt= 'logo' style={{ width: '50%' }}src= {{ logo }}/>
+                </Grid>
+                <Grid item xs={3}>
+                    <CircularProgress style={{ color: '#F97A09' }} />
+                </Grid>
+
+            </Grid>
+
+        </Card>
+    );
+}
+export default Loading;
