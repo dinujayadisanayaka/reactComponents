07ce828b96ded067215cb181a94d61e85b0284f8
dmnddisanayaka <dmnddisanayaka@gmail.com>
2021-10-11 17:04:06 +0530

    validate

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Form from './pages/Form';
 import Pchat from './pages/Pchat';
 import bgraph from './pages/bgraph';
 import {blue} from "@mui/material/colors";
+import Validate from "./components/form/Validate";
 
 
 
@@ -17,6 +18,7 @@ export default class App extends Component {
         <Route path="/form" exact component={Form}></Route>
         <Route path="/pchart" exact component={Pchat}></Route>
         <Route path="/bgraph" exact component={bgraph}></Route>
+        <Route path="/valid" exact component={Validate}></Route>
       </div>
      </BrowserRouter>
     )
